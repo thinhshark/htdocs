ba3e40709023cc84cd53dc19358f5006856d348c
thinhshark <42465850+thinhshark@users.noreply.github.com>
2022-06-09 17:20:38 +0700

    Update single.php

--- udemy/wp-content/themes/udemy/single.php
+++ udemy/wp-content/themes/udemy/single.php
@@ -129,47 +129,66 @@
 
 <div class="related-posts clearfix">
 
+<?php 
+
+            $categories = get_the_category();
+
+            $rp_query = new WP_Query([
+                'post_per_page' => 2,
+                'post__not_in'  => [$post->ID],
+                'cat'           => !empty($categories) ? $categories[0]->term_id : null 
+            ]);
+
+            if($rp_query->have_posts()){
+              while($rp_query->have_posts()){
+                $rp_query->the_post();
+
+                ?>
+                
   <div class="mpost clearfix">
+<?php
+  if(has_post_thumbnail()){
+    ?>
+
     <div class="entry-image">
-      <a href="#">
-        <img src="images/blog/small/10.jpg">
+      <a href="<?php the_permalink(); ?>">
+        <?php the_post_thumbnail('thumbnail'); ?>
       </a>
     </div>
+
+    <?php
+  }
+?>
+
+
     <div class="entry-c">
       <div class="entry-title">
         <h4>
-          <a href="#">
-            This is an Image Post
+          <a href="<?php the_permalink(); ?>">
+            <?php the_title(); ?>
           </a>
         </h4>
       </div>
       <ul class="entry-meta clearfix">
-        <li><i class="icon-calendar3"></i> 10th July 2014</li>
-        <li><i class="icon-comments"></i> 12</li>
+        <li><i class="icon-calendar3"></i> <?php echo get_the_date(); ?></li>
+        <li><i class="icon-comments"></i> <?php echo comments_number('0'); ?></li>
       </ul>
       <div class="entry-content">
-        Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Mollitia nisi perferendis.
+        <?php the_excerpt(); ?>
       </div>
     </div>
   </div>
+                
+                <?php
+              }
 
-  <div class="mpost clearfix">
-    <div class="entry-image">
-      <a href="#"><img src="images/blog/small/20.jpg" alt="Blog Single"></a>
-    </div>
-    <div class="entry-c">
-      <div class="entry-title">
-        <h4><a href="#">This is a Video Post</a></h4>
-      </div>
-      <ul class="entry-meta clearfix">
-        <li><i class="icon-calendar3"></i> 24th July 2014</li>
-        <li><i class="icon-comments"></i> 16</li>
-      </ul>
-      <div class="entry-content">Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Mollitia nisi perferendis.</div>
-    </div>
-  </div>
+              wp_reset_postdata();
+
+            }
+
+?>
+
+
 
 </div>
 
